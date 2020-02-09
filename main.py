46b3d62d39437465980665a8c2ab979edd4dd831
Vismay Vakharia <vismay.vakharia@tcs.com>
2020-02-10 03:53:48 +0530

    added rotation with quaternion implementation

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import enum
 from typing import List, Union, Tuple, Dict, Set
 import itertools
 import numpy as np
+import quaternion
+
+Quaternion = quaternion.quaternion
 
 
 class Vector(object):
@@ -26,7 +29,7 @@ class Vector(object):
         res.z += nz
         return res
 
-    def __mul__(self, other: float):
+    def __mul__(self, other: Union[float, int]):
         return Vector(self.x * other, self.y * other, self.z * other)
 
     def __sub__(self, other):
@@ -53,6 +56,9 @@ class Vector(object):
     def midpoint(self, other: 'Vector'):
         return tuple(i/2 for i in self + other)
 
+    def as_array(self):
+        return np.array([self.x, self.y, self.z])
+
 
 class PieceType(enum.Enum):
     CENTER = 1
@@ -68,14 +74,14 @@ Colors = Tuple[Color, Color, Color, Color, Color, Color]
 
 
 class Piece(object):
-    def __init__(self, _type: PieceType, colors: Colors, position: Vector, orientation: Tuple[Vector, Vector]):
+    def __init__(self, _type: PieceType, colors: Colors, position: Vector, orientation: Quaternion):
         self.type = _type
         self.colors = colors
         self.position = position
         self.orientation = orientation
 
     def __repr__(self):
-        return "{type} piece at {position} with orientations {orientation}".format(**self.__dict__)
+        return "{type} piece at {position} with {orientation}".format(**self.__dict__)
 
 
 class Cube(object):
@@ -85,7 +91,7 @@ class Cube(object):
         assert size != 1, "1x1 cube is not supported yet!"
 
         # corners
-        orient_vec = (Vector(1, 0, 0), Vector(0, 1, 0))
+        orient_vec = lambda: Quaternion(1, 0, 0, 0)
         color_list = (1, 2, 3, 4, 5, 6)
 
         dist = (size - 1) / 2.
@@ -94,7 +100,7 @@ class Cube(object):
         for corner in _corners:
             position_vec = Vector(*corner)
             self.corners.append(Piece(PieceType.CORNER, color_list,
-                                      position_vec, orient_vec))
+                                      position_vec, orient_vec()))
 
         # edges
         _edge_groups = list(itertools.product(*([[-dist, dist]] * 2)))
@@ -110,7 +116,7 @@ class Cube(object):
         for edge in _edges:
             position_vec = Vector(*edge)
             self.edges.append(Piece(PieceType.EDGE, color_list,
-                                    position_vec, orient_vec))
+                                    position_vec, orient_vec()))
 
         # centers
         _center_groups = [-dist, dist]
@@ -128,11 +134,30 @@ class Cube(object):
         for center in _centers:
             position_vec = Vector(*center)
             self.centers.append(Piece(PieceType.CENTER, color_list,
-                                      position_vec, orient_vec))
+                                      position_vec, orient_vec()))
+
+        self.pieces = self.centers + self.corners + self.edges
+
+    def _rotate(self, axis: Vector, multiplier: int = -1):
+        axis = axis.as_array()
+        rotation_vec = axis * multiplier * (np.pi/2)
+        rotation_quat = quaternion.from_rotation_vector(rotation_vec)
+
+        for piece in self.pieces:
+            pos = piece.position.as_array()
+            if np.dot(pos, axis) > 0.5:  # TODO: generalize for nxn cube
+                piece.orientation = rotation_quat * piece.orientation
+                piece.position = Vector(*quaternion.rotate_vectors(rotation_quat, pos))
 
 
 if __name__ == "__main__":
     cube = Cube(3)
-    print(cube.corners, sep="\n")
-    print(cube.edges, sep="\n")
+    print(*cube.corners, sep="\n")
+    print(*cube.edges, sep="\n")
+    print(*cube.centers, sep="\n")
+    cube._rotate(Vector(1, 0, 0), 1)
+    cube._rotate(Vector(0, 1, 0), 1)
+    print("Rotated Cube")
+    print(*cube.corners, sep="\n")
+    print(*cube.edges, sep="\n")
     print(*cube.centers, sep="\n")
